c5ddeb20e8f219c86a2d408a6ec5a0cfab91166c
SkyWeb20 <129883661+SkyWeb20@users.noreply.github.com>
2024-04-19 11:11:22 -0800

    script.js

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ faqQuestions.forEach(question => {
   question.addEventListener('click', () => {
     question.classList.toggle('active');
     const answer = question.nextElementSibling;
+    const iconMinus = question.querySelector('.icon-minus');
+    const iconPlus = question.querySelector('.icon-plus');
     if (answer.style.display === 'none') {
       answer.style.display = 'block';
+      iconMinus.style.display = 'block';
+      iconPlus.style.display = 'none';
     } else {
       answer.style.display = 'none';
+      iconMinus.style.display = 'none';
+      iconPlus.style.display = 'block';
     }
   });
 });
